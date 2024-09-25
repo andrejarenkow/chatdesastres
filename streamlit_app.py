39333e5a2c697b7982617e92ceb57f04a17e2b42
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-09-25 14:41:18 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from pandasai.llm import OpenAI
 from pandasai import Agent
 
 # Leitura do dataframe
-data = pd.read_excel("https://docs.google.com/spreadsheets/d/1mnF0CnGhJD41i7L-FZ1_K1gEhfpIgVmQO_6AEEVFNyM/pub?output=xlsx")
+data = pd.read_excel(st.secrets('link_planilha'))
 
 # filtrando somente Carimbo de data/hora depoius de 2024-09-09
 data = data[data['Carimbo de data/hora'] > '2024-09-09']

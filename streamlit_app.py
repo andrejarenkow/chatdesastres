4b1ab1fe87ffaca4f39c5482dd67a11037ad70b3
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-09-25 15:18:05 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,9 +37,10 @@ llm = OpenAI(
 agente = Agent(data, config={"llm": llm})
 
 prompt = st.chat_input('Pergunte algo sobre as respostas do formulário do Vigidesastres')
-prompt = 'Este dataframe é um formulário de municípios que estão passando por situação de desastre. ' + prompt
+
 #resposta = agente.chat('faça um gráfico de linha do tempo com as respostas')
 if prompt:
+    prompt = 'Este dataframe é um formulário de municípios que estão passando por situação de desastre. ' + prompt
     nova_mensagem = {'role':'user', 'content':prompt}
     chat = st.chat_message(nova_mensagem['role'])
     chat.markdown(nova_mensagem['content'])

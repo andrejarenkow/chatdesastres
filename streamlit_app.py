15a20995056d3b42cadfdf8ee93ac7d27293e7d1
André Jarenkow <45919995+andrejarenkow@users.noreply.github.com>
2024-09-26 22:04:38 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ from pandasai.llm import OpenAI
 from pandasai import Agent
 from pandasai.responses.streamlit_response import StreamlitResponse
 from pandasai.llm import BambooLLM
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain_openai import ChatOpenAI, OpenAI
 
 # Configurações da página
 st.set_page_config(
@@ -18,47 +21,72 @@ st.set_page_config(
 # título
 st.header('Chat Desastres RS', divider=True)
 
-# Verifica se a chave 'mensagens' existe no st.session_state
-if 'mensagens' not in st.session_state:
-        st.session_state.mensagens = []  # Inicializa 'mensagens' como uma lista vazia
+consumer_complaint_data = pd.read_csv('https://docs.google.com/spreadsheets/d/1mnF0CnGhJD41i7L-FZ1_K1gEhfpIgVmQO_6AEEVFNyM/pub?output=csv', parse_dates= ['Carimbo de data/hora'], dayfirst =  True)
+consumer_complaint_data_setembro = consumer_complaint_data[consumer_complaint_data['Carimbo de data/hora']>='2024-09-23']
 
-mensagens = st.session_state['mensagens']  # Acessa a lista de mensagens
+# Save dataset as CSV
+consumer_complaint_data_setembro.to_csv('consumer_complaint_data.csv', index=False, sep=';')
 
-# Leitura do dataframe
-df = pd.read_excel(st.secrets['link_planilha'])
+# Nomes das colunas
+nomes_colunas =         [
+        'Data e hora da resposta',
+        'Coordenadoria Regional de Saúde (CRS)',
+        'Município',
+       'Respondente',
+       'Área técnica',
+       'E-mail',
+       'Finalidade do preenchimento',
+       'Tipologia do desastre',
+       'Data de ocorrência do evento',
+       'Interrupção do fornecimento de energia elétrica',
+       'Interrupção do abastecimento de água',
+       'Fontes de abastecimento alternativas água',
+       'Estabelecimento de saúde afetado',
+       'Descrição estabelecimentos de saúde afetados',
+       'Interrupção dos serviços de saúde',
+       'Perda de medicamentos',
+       'Perda de imunobiológicos',
+       'Descrição dos insumos, medicamentos e imunobiológicos que foram perdidos',
+       'Abrigos públicos utilizados',
+       'Trabalhadores/voluntários tem acesso aos EPIs necessários para atuar nessa emergência? ',
+       'Óbitos',
+       'Possiblidade de abrigos',
+       'O município possui Plano de Contigência ou Ação, no âmbito da saúde, para desastres?',
+       'Perda de insumos',
+       'Quais são esses abrigos? Descreva o local e sua capacidade de ocupação. Se possível informe o número de pessoas abrigadas (Ex: Ginásio municipal ...)']
 
-# filtrando somente Carimbo de data/hora depoius de 2024-09-09
-df = df[df['Carimbo de data/hora'] >= '2024-09-23']
 
-# Instantiate a LLM
-#llm = OpenAI(
-#    api_token=st.secrets['OPENAI_API_KEY'],
-#    temperature=0,
-#)
-llm =  BambooLLM(api_key=st.secrets["BAMBOO_API_KEY"])
-agente = SmartDataframe(df,
-               config={'llm':llm,
-                       "verbose": True,
-                       "response_parser": StreamlitResponse})
+csv_agent = create_csv_agent(
+    ChatOpenAI(temperature=0, model="gpt-4o-mini-2024-07-18", api_key=st.secrets["OPENAI_API_KEY"]),
+    path='/content/consumer_complaint_data.csv',
+    pandas_kwargs={'sep': ';', 'parse_dates': ['Data e hora da resposta'], 'dayfirst': True, 'names':nomes_colunas, 'header':0},
+    allow_dangerous_code=True,
+    verbose=True,
+    include_df_in_prompt  = 10,
+    agent_type = AgentType.OPENAI_FUNCTIONS,
+    prefix='Este é um dataframe chamado df a partir de um Google Forms, onde as respostas são relacionadas aos municípios que passam por situação de desastre'
+)
+
 
 prompt_usuario = st.chat_input('Pergunte algo sobre as respostas do formulário do Vigidesastres')
 
+resposta = csv_agent.invoke({'input': 'quais municípios tiveram problema com abastecimento de água?'})
+print(resposta['output'])
+
 #resposta = agente.chat('faça um gráfico de linha do tempo com as respostas')
 if prompt_usuario:
-    prompt = 'Este dataframe é um formulário de municípios que estão passando por situação de desastre.' + prompt_usuario + ' Dê a resposta em markdown. Essa resposta será mostrada em um webapp do streamlit'
     nova_mensagem = {'role':'user', 'content':prompt_usuario}
     chat = st.chat_message(nova_mensagem['role'])
     chat.markdown(nova_mensagem['content'])
-    #mensagens.append(nova_mensagem)
 
     # Gerar resposta do modelo da tabela
     chat = st.chat_message('assistant')
     placeholder = chat.empty()
-    resposta_completa = agente.chat(prompt)
+    resposta_completa = csv_agent.invoke({'input': prompt_usuario})['output']
     if resposta_completa.endswith(".png"):
         st.image(resposta_completa)
     else:
-        st.markdown(resposta_completa)
+        st.write(resposta_completa)
 
 
     
